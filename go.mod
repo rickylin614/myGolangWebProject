970119641e2fc14712270305acfacca29bba1cbf
ricky.lin <ricky.lin@timecitytech.com>
2021-02-17 17:31:37 +0800

    設定檔更改為絕對路徑

--- go.mod
+++ go.mod
@@ -15,8 +15,6 @@ require (
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/magiconair/properties v1.8.4 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/onsi/ginkgo v1.14.2 // indirect
 	github.com/onsi/gomega v1.10.4 // indirect
 	github.com/pelletier/go-toml v1.8.1 // indirect
@@ -29,7 +27,6 @@ require (
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.16.0
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
-	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb
 	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
 	golang.org/x/text v0.3.5 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
--- src/utils/viperUtils/config.go
+++ src/utils/viperUtils/config.go
@@ -1,19 +1,49 @@
 package viperUtils
 
 import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+
 	"github.com/spf13/viper"
 )
 
-const basepath = "./resource/properties/"
+// go get github.com/spf13/viper
+
+const basepath = "../../../resource/properties/"
 const commonFile = basepath + "common.properties"
 const logPathParam = "logPath"
 
-// go get github.com/spf13/viper
+var currentBase string
+
+func init() {
+	_, currentFile, _, _ := runtime.Caller(0)
+	currentBase = filepath.Dir(currentFile)
+}
+
 func GetLogPath() string {
-	viper.SetConfigFile(commonFile)
+	path := AbsolutePath(commonFile)
+	fmt.Println(path)
+	viper.SetConfigFile(path)
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(err)
 	}
 	return viper.GetString(logPathParam)
+}
+
+func GetCommonParams(param string) string {
+	path := AbsolutePath(commonFile)
+	fmt.Println(path)
+	viper.SetConfigFile(path)
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("err", err)
+		return ""
+	}
+	return viper.GetString(param)
+}
+
+func AbsolutePath(rel string) string {
+	return filepath.Join(currentBase, rel)
 }
